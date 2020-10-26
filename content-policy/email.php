7170e47b3e54e2f08dc3661f503521d531096e6c
Benjamin Newman <benewman2002@gmail.com>
2020-10-26 13:18:54 +0000

    phpT8

--- content-policy/email.php
+++ content-policy/email.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
 <html>
     <body>
-        <p>Debug v7</p>
+        <p>Debug v8</p>
     <?php
 
-        echo $_GET["p1"];
+        echo $_POST["p1"];
 
-        $firstName = $_GET["p1"];
-        $lastName = $_GET["p2"];
-        $emailAddress = $_GET["p3"];
-        $reason = $_["p4"];
-        $messageRaw = $_["p5"];
+        $firstName = $_POST["p1"];
+        $lastName = $_POST["p2"];
+        $emailAddress = $_POST["p3"];
+        $reason = $_POST["p4"];
+        $messageRaw = $_POST["p5"];
 
         $messageSend = "Name: {$firstName} {$lastName}\n
                         Email address: {$emailAddress}\n
@@ -20,7 +20,7 @@
 
         mail("teams@opensauce.uk", "Message from opensauce", $messageSend);
 
-        echo "Debug v7" <br>
+        echo "Debug v8" <br>
 
         echo "The Following message has been sent:\n";
         print $messageSend;
