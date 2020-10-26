86b2c7e2e2f9d58cf3d0bcc3f02359d3cbd16f38
Benjamin Newman <benewman2002@gmail.com>
2020-10-26 12:52:07 +0000

    testing php

--- content-policy/email.php
+++ content-policy/email.php
@@ -1,14 +1,25 @@
 <!DOCTYPE html>
 <html>
     <body>
-
-    <h1>PHP Test</h1>
-
     <?php
 
-    echo "Hello World!";
-    
-    ?>
+        $firstName = $_GET["p1"];
+        $lastName = $_GET["p2"];
+        $emailAddress = $_GET["p3"];
+        $reason = $_["p4"];
+        $messageRaw = $_["p5"];
 
+        $messageSend = "Name: {$firstName} {$lastName}\n
+                        Email address: {$emailAddress}\n
+                        Reason for contact {$reason}\n
+                        Message Content:\n\n{$messageRaw}";
+
+
+        mail("teams@opensauce.uk", "Message from opensauce", $messageSend);
+
+        echo "The Following message has been sent:\n";
+        echo $messageSend;
+
+    ?>
     </body>
 </html> 
