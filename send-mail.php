56f6e3d5a4d830d036d4a60bcd3019fc12b23481
Benjamin Newman <benewman2002@gmail.com>
2020-10-26 14:00:56 +0000

    recaptcha testing

--- send-mail.php
+++ send-mail.php
@@ -4,16 +4,34 @@
         <p>Debug v11</p>
     <?php
 
+        echo $_POST["g-recaptcha-response"];
+
 
         if (!isset($_POST["p6"]) || $_POST["p6"] != "botcheck") {  
-            header("Location: ./contact.html");
+            header("Location: contact.html");
             exit;  
         }
+
+        if ("POST" != getenv("REQUEST_METHOD")) {  
+            header("Location: contact.html");
+            exit;  
+        }
+
+        if ("" == getenv("HTTP_USER_AGENT") || "" == getenv("HTTP_REFERER")) {
+            header("Location: contact.html");
+            exit;  
+        }
+
+        if (!empty($_POST["email"])) 
+            exit;  
+
 
         echo $_POST["p1"];
         $firstName = $_POST["p1"];
         $lastName = $_POST["p2"];
         $emailAddress = $_POST["p3"];
+        if (!preg_match("/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/", $emailAddress))
+            exit;
         $reason = $_POST["p4"];
         $messageRaw = $_POST["p5"];
 
