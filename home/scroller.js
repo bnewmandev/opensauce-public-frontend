607f3ebd9df68a5061d5a3db3ce348c3f46bc425
Benjamin Newman <benewman2002@gmail.com>
2020-10-28 13:00:00 +0000

    Homepage updated

--- home/scroller.js
+++ home/scroller.js
@@ -6,5 +6,11 @@ function RecentRight() {
     document.getElementById("recent-posts-js").scrollLeft += 250;
     return 200;
   };
+function IngredientsLeft() {
+  document.getElementById("ingredients-js").scrollLeft -= 250;
+}
 
-  
+function IngredientsRight() {
+  document.getElementById("ingredients-js").scrollLeft += 250;
+  return 200;
+};
