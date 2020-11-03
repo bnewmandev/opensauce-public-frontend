f86a4375d0ea320d9409f45bfe791d4187e0903f
Benjamin Newman <benewman2002@gmail.com>
2020-11-03 02:01:09 +0000

    footer work

--- home/scroller.js
+++ home/scroller.js
@@ -1,25 +1,25 @@
 function RecentLeft() {
-    document.getElementById("recent-posts-js").scrollLeft -= 250;
+    document.getElementById("recent-posts-js").scrollLeft -= 500;
     return 200;
   };
 function RecentRight() {
-    document.getElementById("recent-posts-js").scrollLeft += 250;
+    document.getElementById("recent-posts-js").scrollLeft += 500;
     return 200;
   };
 function IngredientsLeft() {
-  document.getElementById("ingredients-js").scrollLeft -= 250;
+  document.getElementById("ingredients-js").scrollLeft -= 500;
 }
 
 function IngredientsRight() {
-  document.getElementById("ingredients-js").scrollLeft += 250;
+  document.getElementById("ingredients-js").scrollLeft += 500;
   return 200;
 };
 
 function RecipesLeft() {
-  document.getElementById("recipes-js").scrollLeft -= 250;
+  document.getElementById("recipes-js").scrollLeft -= 500;
 }
 
 function RecipesRight() {
-  document.getElementById("recipes-js").scrollLeft += 250;
+  document.getElementById("recipes-js").scrollLeft += 500;
   return 200;
 };
